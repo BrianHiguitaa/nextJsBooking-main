9718ce51dc7e8547be5e5156faf0b2a03a0ff5c0
brian <brian-higuita@hotmail.co.uk>
2024-03-15 22:56:12 +0000

    basic setup with footer and navigation

---
+++ pages/index.js
@@ -0,0 +1,47 @@
+import Head from "next/head";
+
+
+import {
+  useStoryblokState,
+  getStoryblokApi,
+  StoryblokComponent,
+} from "@storyblok/react";
+import Layout from "../components/Layout";
+
+export default function Home({ story }) {
+  story = useStoryblokState(story);
+
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    <Layout>
+      <StoryblokComponent blok={story.content} />
+    </Layout>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  let slug = "home";
+
+  let sbParams = {
+    version: "draft", // or 'published'
+    resolve_links: "url",
+  };
+
+  const storyblokApi = getStoryblokApi();
+  let { data } = await storyblokApi.get(`cdn/stories/${slug}`, sbParams);
+  let { data: config } = await storyblokApi.get('cdn/stories/config');
+
+  return {
+    props: {
+      story: data ? data.story : false,
+      key: data ? data.story.id : false,
+      config: config ? config.story : false,
+    },
+    revalidate: 3600,
+  };
+}
